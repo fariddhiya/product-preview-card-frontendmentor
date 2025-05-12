eb31440357400acd1ff57ad4ef2d19e5517bbcf7
Farid Dhiya <farid@mareco.id>
2025-05-12 22:55:57 +0700

    desktop version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex h-screen w-full items-center justify-center overflow-hidden bg-[var(--cream)] p-5">
+	<div
+		class="flex items-center justify-center overflow-hidden !rounded-xl !border-0 !p-0 shadow-lg sm:h-full sm:w-full sm:flex-col lg:h-[62%] lg:w-[40%] lg:flex-row"
+	>
+		<div class="image flex !overflow-hidden bg-red-700 sm:h-1/3 sm:w-full lg:h-full lg:w-1/2">
+			<img src="image-product-desktop.jpg" alt="" />
+		</div>
+		<div
+			class="!sm:h-2/3 flex w-1/2 flex-col justify-between !overflow-hidden bg-white p-8 lg:h-full"
+		>
+			<h5 class="tracking-[0.5rem] text-[var(--grey)] uppercase">Perfume</h5>
+			<h1 class="text-5xl font-bold">Gabrielle Essence Eau De Parfum</h1>
+			<p class="text-lg leading-7 tracking-[0.04rem] text-[var(--grey)]">
+				A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator
+				for the House of CHANEL.
+			</p>
+			<div class="flex items-center">
+				<p class="pr-4 text-4xl font-bold text-[var(--green-500)]">$149.99</p>
+				<p class="text-[var(--grey)] line-through">$169.99</p>
+			</div>
+			<button class="flex h-14 w-5/6 items-center justify-center rounded-lg bg-[var(--green-700)]">
+				<img src="icon-cart.svg" alt="" sizes="100rem" class="h-6 w-6 font-bold" />
+				<p class="pl-2 font-bold text-white">Add to Cart</p>
+			</button>
+		</div>
+	</div>
+	<div class="absolute bottom-2 text-lg">Challenge by Frontend Mentor. Coded by Farid Dhiya.</div>
+</div>
