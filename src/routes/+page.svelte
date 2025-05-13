6694cb9bee400a06cf350f9968b5d81e4dd941b4
Farid Dhiya <farid@mareco.id>
2025-05-13 16:29:49 +0700

    refactor: update package dependencies and improve CSS structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,25 @@
-<div class="flex h-screen w-full items-center justify-center overflow-hidden bg-[var(--cream)] p-5">
+<div
+	class="flex h-screen w-full items-center justify-center overflow-hidden bg-[var(--cream)] p-5 sm:py-15"
+>
 	<div
-		class="flex items-center justify-center overflow-hidden !rounded-xl !border-0 !p-0 shadow-lg sm:h-full sm:w-full sm:flex-col lg:h-[62%] lg:w-[40%] lg:flex-row"
+		class="flex items-center justify-center overflow-hidden rounded-xl border-0 p-0 shadow-lg sm:h-11/12 sm:w-full sm:flex-col md:h-2/3 md:w-2/3 lg:h-[80%] lg:flex-row xl:h-[75%] xl:w-[40%]"
 	>
-		<div class="image flex !overflow-hidden bg-red-700 sm:h-1/3 sm:w-full lg:h-full lg:w-1/2">
-			<img src="image-product-desktop.jpg" alt="" />
+		<div class="flex bg-red-700 sm:h-1/3 sm:w-full lg:h-full lg:w-1/2 lg:overflow-hidden">
+			<img
+				src="image-product-desktop.jpg"
+				class="object-cover sm:h-full sm:w-full"
+				alt="Gabrielle Essence Eau De Parfum bottle"
+				loading="lazy"
+			/>
 		</div>
 		<div
-			class="!sm:h-2/3 flex w-1/2 flex-col justify-between !overflow-hidden bg-white p-8 lg:h-full"
+			class="flex flex-col justify-between overflow-hidden bg-white p-8 sm:h-full sm:w-full lg:h-full lg:w-1/2"
 		>
 			<h5 class="tracking-[0.5rem] text-[var(--grey)] uppercase">Perfume</h5>
-			<h1 class="text-5xl font-bold">Gabrielle Essence Eau De Parfum</h1>
-			<p class="text-lg leading-7 tracking-[0.04rem] text-[var(--grey)]">
+			<h1 class="font-bold sm:text-3xl md:text-5xl lg:text-4xl xl:text-5xl">
+				Gabrielle Essence Eau De Parfum
+			</h1>
+			<p class="text-[1.1rem] leading-7 tracking-[0.035rem] text-[var(--grey)]">
 				A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator
 				for the House of CHANEL.
 			</p>
@@ -18,11 +27,16 @@
 				<p class="pr-4 text-4xl font-bold text-[var(--green-500)]">$149.99</p>
 				<p class="text-[var(--grey)] line-through">$169.99</p>
 			</div>
-			<button class="flex h-14 w-5/6 items-center justify-center rounded-lg bg-[var(--green-700)]">
-				<img src="icon-cart.svg" alt="" sizes="100rem" class="h-6 w-6 font-bold" />
+			<button
+				aria-label="Add Gabrielle Essence Eau De Parfum to cart"
+				class="flex w-full items-center justify-center rounded-lg bg-[var(--green-700)] hover:cursor-pointer hover:bg-[var(--green-500)] sm:h-10 lg:h-12 xl:h-14"
+			>
+				<img src="icon-cart.svg" alt="" sizes="100rem" class="h-5 w-5 font-bold" />
 				<p class="pl-2 font-bold text-white">Add to Cart</p>
 			</button>
 		</div>
 	</div>
-	<div class="absolute bottom-2 text-lg">Challenge by Frontend Mentor. Coded by Farid Dhiya.</div>
+	<div class="absolute bottom-2 text-center sm:text-sm lg:text-sm">
+		Challenge by Frontend Mentor. Coded by Farid Dhiya.
+	</div>
 </div>
